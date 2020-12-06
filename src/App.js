bb76a9c5ad6ac7d89288642abaecdddebd70ed7b
IonSibRe <ionsibre@gmail.com>
2020-12-06 19:13:04 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home, About, Products, Contact, Error } from "./pages";
+import Navbar from "./components/Navbar";
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<Navbar />
+					<Home />
+				</Route>
+				<Route path="/about">
+					<Navbar />
+					<About />
+				</Route>
+				<Route path="/products">
+					<Navbar />
+					<Products />
+				</Route>
+				<Route path="/contact">
+					<Navbar />
+					<Contact />
+				</Route>
+				<Route path="*">
+					<Error />
+				</Route>
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
---
+++ src/pages/Error.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/error/error.scss";
+
+function Error() {
+	return (
+		<main className="main-container">
+			<div className="error-text-wrap">
+				<h1 className="error-title">404</h1>
+				<h3 className="error-subtitle">
+					Sorry, the page you are looking for doesn't exist
+				</h3>
+				<Link to="/" className="home-btn">
+					Back Home
+				</Link>
+			</div>
+		</main>
+	);
+}
+
+export default Error;
